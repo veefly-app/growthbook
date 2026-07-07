dd28b637b8fa155b33054f14a7c96613599a00d5
Chinmay Chaudhari <chinmay.chaudhari@veefly.com>
2026-07-08 00:48:44 +0530

    license url commented

--- packages/back-end/src/enterprise/licenseUtil.ts
+++ packages/back-end/src/enterprise/licenseUtil.ts
@@ -30,8 +30,8 @@ import { getLicenseByKey, LicenseModel } from "./models/licenseModel";
 import { LICENSE_PUBLIC_KEY } from "./public-key";
 
 export const LICENSE_SERVER_URL =
-  process.env.LICENSE_SERVER_URL ||
-  "https://central-license-server.growthbook.io/api/v1/";
+  process.env.LICENSE_SERVER_URL || "";
+  // "https://central-license-server.growthbook.io/api/v1/";
 
 // mimic behavior in back-end/src/util/secrets.ts
 const APP_ORIGIN = process.env.APP_ORIGIN || "http://localhost:3000";
